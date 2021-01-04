68ac9f6a3813e20c489c2ef2f1c72bf083a87b1b
Doncho Gunchev <dgunchev@gmail.com>
2021-01-04 17:01:54 +0200

    Add compiler info (and debug/release)

--- main.cpp
+++ main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << "\ncompiler version " << __VERSION__;
+#ifndef NDEBUG
+    cout << ", with debug info/stl checks (no NDEBUG defined).";
+#endif
+    cout << endl;
     return 0;
 }
